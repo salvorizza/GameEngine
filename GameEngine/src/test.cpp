362355df4dd3f26e6576a368498de529c8dbf15d
salvorizza <40764521+salvorizza@users.noreply.github.com>
2018-10-23 19:15:34 +0200

    First commit

---
+++ GameEngine/src/test.cpp
@@ -0,0 +1,11 @@
+#include "test.h"
+
+#include<stdio.h>
+
+namespace GameEngine {
+
+	void Print() {
+		printf("Hello to Game Engine\n");
+	}
+
+}
---
+++ GameEngine/src/test.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+namespace GameEngine {
+
+	_declspec(dllexport) void Print();
+
+}
